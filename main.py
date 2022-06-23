6580b71a7e7402c0b8e02e220d04b524d1239244
halilmuslu <halilmuslu@norbit.com.tr>
2022-06-23 10:24:34 +0300

    matematik classı oluşturuldu. Topla fonksiyonu yazıldı.

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+
+class Matematik:
+    def __init__(self, sayi_1, sayi_2):
+        self.sayi_1 = sayi_1
+        self.sayi_2 = sayi_2
+
+    def topla(self):
+        return (self.sayi_1 + self.sayi_2)
+
